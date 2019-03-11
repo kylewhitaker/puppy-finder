ce6a94f4466a4d62d81fac4c4b3f05db71a7b5aa
Kyle Whitaker <kylewhitaker51@gmail.com>
2019-03-11 13:44:11 -0400

    use mongoose to fetch data from DB

--- server/server.js
+++ server/server.js
@@ -1,9 +1,43 @@
+// Connect to MongoDB
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/puppy-finder', (error) => {
+  if (error) {
+    console.error('MongoDB is not running. Failed to connect.');
+    throw error;
+  } else {
+    console.log('Successfully connected to MongoDB');
+  }
+});
+
+// Create DB Model
+const Schema = mongoose.Schema;
+const canineSchema = new Schema({
+  type: 'String',
+  age: 'String',
+  coordinates: [['String']]
+});
+const doggieSchema = new Schema({
+  title: 'String',
+  image: 'String',
+  canines: [canineSchema]
+});
+const model = mongoose.model('Puppy', doggieSchema, 'puppies');
+
+// Startup Express
 const express = require('express');
 const app = express();
 
+// Setup API endpoint, fetch puppy data from DB
 app.get('/', (req, res) => {
-  res.send('Find some puppies!');
-})
+  model.find().exec((err, puppies) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.json(puppies);
+    }
+  });
+});
 
+// Serve port
 const port = 8000;
 app.listen(port, () => console.log(`Server listening on port ${port}!`));
