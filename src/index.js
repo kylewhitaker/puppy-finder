2969953f62dbcc6540e8536cddef631a955ef274
Kyle Whitaker <kylewhitaker51@gmail.com>
2019-03-11 14:32:13 -0400

    add table below photo

--- src/index.js
+++ src/index.js
@@ -5,12 +5,50 @@ import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "./index.css";
 
+
 const getData = () => {
   return fetch(`http://localhost:8000/`, { headers: { 'content-type': 'application/json' }, method: 'get' })
     .then(response => response.json().then(json => ({ json, response })))
     .then(({ json, response }) => !response.ok ? Promise.reject(json) : json)
     .then(response => response, error => error);
 }
+
+
+class Table extends React.Component {
+
+  render() {
+    let groups = [];
+    this.props.canines.forEach(canine => {
+      const idx = groups.findIndex(x => x.type === canine.type && x.age === canine.age);
+      if (idx < 0) {
+        groups.push({ type: canine.type, age: canine.age, qty: 1 });
+      } else {
+        groups[idx].qty++;
+      }
+    })
+    const rows = groups.map(canine => {
+      return (
+        <tr>
+          <td>{canine.type}</td>
+          <td>{canine.age}</td>
+          <td>{canine.qty}</td>
+        </tr>
+      )
+    })
+    return (
+      <table>
+        <tr>
+          <th>Type</th>
+          <th>Age</th>
+          <th>Qty</th>
+        </tr>
+        {rows}
+      </table>
+    );
+  }
+
+}
+
 
 class App extends React.Component {
 
@@ -22,7 +60,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="container">
-        <Carousel showThumbs={false}>
+        <Carousel width="720px" showThumbs={false}>
           {this.state.puppies}
         </Carousel>
       </div>
@@ -36,6 +74,7 @@ class App extends React.Component {
           <div>
             <p className="title">{puppy.title}</p>
             <img src={puppy.image}></img>
+            <Table canines={puppy.canines}></Table>
           </div>
         )
       });
@@ -44,5 +83,6 @@ class App extends React.Component {
   }
 
 }
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
