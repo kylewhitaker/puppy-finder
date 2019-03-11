02a7e05d401567e3503be37220a8a3b5f11d862e
Kyle Whitaker <kylewhitaker51@gmail.com>
2019-03-11 14:14:20 -0400

    add photo carousel

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import fetch from 'isomorphic-fetch';
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import "./index.css";
 
 const getData = () => {
   return fetch(`http://localhost:8000/`, { headers: { 'content-type': 'application/json' }, method: 'get' })
@@ -11,18 +14,33 @@ const getData = () => {
 
 class App extends React.Component {
 
+  constructor() {
+    super();
+    this.state = { puppies: [] };
+  }
+
   render() {
     return (
-      <div>Puppy Finder</div>
+      <Carousel width="720px" showThumbs={false}>
+        {this.state.puppies}
+      </Carousel>
     );
   }
 
   componentDidMount() {
     getData().then(response => {
-      console.log(response);
+      const puppies = response.map(puppy => {
+        return (
+          <div>
+            <p className="title">{puppy.title}</p>
+            <img src={puppy.image}></img>
+          </div>
+        )
+      });
+      this.setState({ puppies });
     });
   }
-  
+
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
